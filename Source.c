4d9a2337b04c8ae075580f712804ca85d78811d2
Roman <molnar.roman619@gmail.com>
2018-12-05 23:58:23 +0100

    Refaktorovanie
    Vizualna uprava kodu. Odstranenie prebytocnych vypisov, odriadkovanie...

--- Source.c
+++ Source.c
@@ -17,14 +17,11 @@
 {
 	FILE *f;
 	f = *f1;
-	int n;
+	int n = 0;
 	char x, c;
 	AUTO *aktualny;
 
- 	//if (*prvy != NULL) uvolni(*prvy);
-
- 	n = 0;
-	aktualny = (AUTO*)malloc(sizeof(AUTO));										//predpokladam ze subor nie je prazdny				
+ 	aktualny = (AUTO*)malloc(sizeof(AUTO));										//predpokladam ze subor nie je prazdny				
 	*prvy = aktualny;
 	if ((f = fopen("auta.txt", "r")) == NULL)									//otvaranie suboru
 	{
@@ -38,17 +35,21 @@
 			x = getc(f);
 			if (x != '$') break;												//pozeram sa ci tam je zaznam
 			c = getc(f);
-			if (n > 0) {														//vytvaranie dalsieho zaznamu ak je potrebne
+
+ 			if (n > 0) 
+			{														//vytvaranie dalsieho zaznamu ak je potrebne
 				aktualny->dalsi = (AUTO*)malloc(sizeof(AUTO));
 				aktualny = aktualny->dalsi;
 			}
-			
-			fgets(aktualny->kategoria, 52, f);									//nacitanie jednotlivych poloziek do zoznamu
+
+ 			fgets(aktualny->kategoria, 52, f);									//nacitanie jednotlivych poloziek do zoznamu
 			fgets(aktualny->znacka, 52, f);										//pouzivam fgets na nacitanie stringu aj s medzerou - ak sa tam nachadza
 			fgets(aktualny->predajca, 102, f);
-			fscanf(f, "%d", &aktualny->cena);
+
+ 			fscanf(f, "%d", &aktualny->cena);
 			fscanf(f, "%d", &aktualny->datum_v);
-			c = getc(f);
+
+ 			c = getc(f);
 			fgets(aktualny->stav, 202, f);
 			aktualny->dalsi = NULL;
 			n++;
@@ -62,10 +63,10 @@
  void vypis(AUTO **prvy)
 {
 	AUTO *p;
+	int n = 1;
 	p = *prvy;
-	int n;
-	n = 1;
-	while (p != NULL)															//prejdem cely zoznam
+
+ 	while (p != NULL)															//prejdem cely zoznam
 	{
 		printf("%d.\nkategoria: %sznacka: %spredajca: %s", n, p->kategoria, p->znacka, p->predajca);
 		printf("cena: %d\nrok_vyroby: %d\nstav_vozidla: %s", p->cena, p->datum_v, p->stav);
@@ -76,20 +77,24 @@
 
  void pridaj(AUTO **prvy)
 {
-	AUTO *p,*pred = NULL,*pridaj;
+	AUTO *p, *pred = NULL, *pridaj;
 	int pozicia, n = 1, pom = 0;
 	char c;
 	p = *prvy;
 	pridaj = (AUTO*)malloc(sizeof(AUTO));										//nacitam pridavanu polozku
-	scanf("%d", &pozicia);
-	c = getchar();																//osetrenie proti nacitaniu konca riadku
-	fgets(pridaj->kategoria, 52, stdin);
+
+ 	scanf("%d", &pozicia);
+	c = getchar();				
+												//osetrenie proti nacitaniu konca riadku
+ 	fgets(pridaj->kategoria, 52, stdin);
 	fgets(pridaj->znacka, 52, stdin);
-	fgets(pridaj->predajca, 52, stdin);
-	scanf("%d", &pridaj->cena);
+	fgets(pridaj->predajca, 102, stdin);
+
+ 	scanf("%d", &pridaj->cena);
 	scanf("%d", &pridaj->datum_v);
-	c = getchar();
-	fgets(pridaj->stav, 52, stdin);
+
+ 	c = getchar();
+	fgets(pridaj->stav, 202, stdin);
 
  	if ((pozicia == 1) || (*prvy == NULL))										//na 1. poziciu ide ak je pozicia 1, resp. v zozname sa nic nenachadza
 	{
@@ -105,7 +110,7 @@
 				pred->dalsi = pridaj;
 				pridaj->dalsi = p;
 				pom = 1;
-				break;	
+				break;
 			}
 			else
 			{
@@ -121,23 +126,19 @@
 		pridaj->dalsi = NULL;
 	}
 
-
  }
+
  int porovnaj(char *slovo1, char *slovo2)										//porovnavam, ci sa v stringu nachadza substring
 {
-	int i, j, k = 0;
-
- 	i = 0;
-	j = 0;
+	int i = 0, j = 0, k = 0;
 
  	while (slovo1[i] != '\n')													//kazdy string v zozname obsahuje \n a idem len poò, kvoli tomu, ze substring sa nemusi nachadzat na konci stringu
 	{
-		if ((slovo1[i] == slovo2[j]) || (slovo1[i] == (slovo2[j]+32)) || (slovo1[i] == (slovo2[j] - 32)))		//podmienka prejde, ak su pismena rovnake bez ohladu na to, ci je to male alebo velke pismeno
+		if ((slovo1[i] == slovo2[j]) || (slovo1[i] == (slovo2[j] + 32)) || (slovo1[i] == (slovo2[j] - 32)))		//podmienka prejde, ak su pismena rovnake bez ohladu na to, ci je to male alebo velke pismeno
 			while (slovo2[j] != '\n')
 			{
 				k = 1;
-				//j++;
-				if (((slovo1[i+j]%65)%32) != ((slovo2[j]%65)%32))				//ak sa nejaka cast substringu nerovna s povodnym - bez ohladu na to ci je male alebo velke, tak tak sa druhy cyklus breakne
+				if (((slovo1[i + j] % 65) % 32) != ((slovo2[j] % 65) % 32))				//ak sa nejaka cast substringu nerovna s povodnym - bez ohladu na to ci je male alebo velke, tak tak sa druhy cyklus breakne
 				{
 					k = 2;
 					j = 0;
@@ -148,7 +149,6 @@
 		if (k == 1)break;														//ak som ho tam nasiel - presiel vnoreny cyklus az do konca a nenasiel chybu, tak skonci
 		i++;
 	}
-
 
  	return k;
 }
@@ -162,15 +162,14 @@
 
  	c = getchar();
 	fgets(slovo, 52, stdin);
-	
-	printf("%s", slovo);
-	while (p != NULL)
+
+ 	while (p != NULL)
 	{
 		if (porovnaj(p->znacka, slovo) == 1)									//ak sa tam subtring nachadza tak ho vypojim
 		{
 			k++;
 			if (pred == NULL)													//ak mazem prvy musim posunut hlavny pointer
-			{ 
+			{
 				*prvy = p->dalsi;
 				free(p);														//a freenem ho
 				p = *prvy;
@@ -205,19 +204,22 @@
 		}
 		i++;
 	}
-	return k;
+
+ 	return k;
 }
 
  void hladaj(AUTO **prvy)
 {
 	AUTO *p;
-	char znacka[52],c;
-	int i = 1,suma;
-	p = *prvy;
+	char znacka[52], c;
+	int i = 1, suma;
+
+ 	p = *prvy;
 	c = getchar();
 	fgets(znacka, 52, stdin);
 	scanf("%d", &suma);
-	while (p != NULL)
+
+ 	while (p != NULL)
 	{
 		if ((zhoda(p->znacka, znacka) == 1) && (suma >= p->cena))				//porovnavam ci su stringy zhodne a ci je cena nejakeho auta mensia ako zadana suma
 		{
@@ -227,7 +229,7 @@
 		}
 		p = p->dalsi;
 	}
-	if (i == 0)printf("V ponuke nie su pozadovane auta\n");
+	if (i == 1) printf("V ponuke nie su pozadovane auta\n");
 }
 
  void aktualizuj(AUTO **prvy)
@@ -240,7 +242,8 @@
  	c = getchar();
 	fgets(znacka, 52, stdin);
 	scanf("%d", &rok);
-	while (p != NULL)
+
+ 	while (p != NULL)
 	{
 		if ((zhoda(p->znacka, znacka) == 1) && (rok == p->datum_v))				//pozeram sa ci sa tam nejake take auto nenachadza
 		{
@@ -255,9 +258,8 @@
 
  void uvolni(AUTO **prvy)														//vlastna funkcia na uvolnenie celeho zoznamu
 {
-	AUTO *p,*pom = NULL;
-
- 	p = *prvy;
+	AUTO *p, *pom = NULL;
+	p = *prvy;
 
  	while (p != NULL)															//uvolnujem kazdy jeden zaznam, nestaci iba prvy smernik
 	{
@@ -298,26 +300,6 @@
 			koniec();
 		}
 	}
-
-
- 	/*
-	nacitaj(&f, &prvy);
-	vypis(&prvy);
-
- 	uvolni(&prvy);
-	//nacitaj(&f, &prvy);
-	//uvolni(&prvy);
-
- 	vypis(&prvy);
-	pridaj(&prvy);
-	vypis(&prvy);
-	zmaz(&prvy);
-	vypis(&prvy);
-
- 	hladaj(&prvy);
-
- 	aktualizuj(&prvy);
-	vypis(&prvy);*/
 
  	return 0;
 }
